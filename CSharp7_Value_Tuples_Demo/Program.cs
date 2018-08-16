a10690b75ebac2d368c157f933809176507f697d
Frans Hendrik Brink <henk.brink1985@gmail.com>
2018-08-16 19:33:45 +1200

    Add project files.

---
+++ CSharp7_Value_Tuples_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp7_Value_Tuples_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
